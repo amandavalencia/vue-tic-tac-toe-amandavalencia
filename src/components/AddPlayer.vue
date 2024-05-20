1ee19b83230957f07f0c90c1d1d492a55fc1e1e5
amandaavalencia <amandaavalencia@hotmail.com>
2024-05-20 09:46:02 +0200

    created button component

---
+++ src/components/AddPlayer.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Button from './Button.vue';
+
+const buttonText = "Add Player"
+
+</script>
+
+<template>
+    <p>Player 1: </p>
+    <input type="text">
+    <Button :my-button="buttonText"/>
+    
+</template>
+
+<style scoped></style>
---
+++ src/components/Tic-Tac-Toe.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import AddPlayer from './AddPlayer.vue';
+
+</script>
+
+<template>
+    <AddPlayer/>
+
+</template>
+<style scoped></style>
